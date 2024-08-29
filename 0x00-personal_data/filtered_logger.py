b5bc9bbea31bd83ecd662d5c4edb941915ff93fb
mathewkoech <mathewkoech55@gmail.com>
2024-08-29 14:29:28 +0000

    function called filter_datum that returns the log message obfuscated

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""A module for filtering logs.
+"""
+import os
+import re
+import logging
+import mysql.connector
+from typing import List
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """                                                                                                                                               
+    Filters a log line by redacting specified fields.                                                                                                 
+                                                                                                                                                      
+    Args:                                                                                                                                             
+        fields (List[str]): The fields to redact.                                                                                                     
+        redaction (str): The string to replace the field values with.                                                                                 
+        message (str): The log message to filter.                                                                                                     
+        separator (str): The separator used in the log message.                                                                                       
+                                                                                                                                                      
+    Returns:                                                                                                                                          
+        str: The filtered log message.                                                                                                                
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats a LogRecord, redacting specified fields.
+
+        Args:
+            record (logging.LogRecord): The log record to format.
+
+        Returns:
+            str: The formatted and redacted log message.
+        """
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
