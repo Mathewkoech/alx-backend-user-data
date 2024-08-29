5dcd83f3899862dfbf373cbc81bcd88e25b3079f
mathewkoech <mathewkoech55@gmail.com>
2024-08-29 14:33:17 +0000

    function called filter_datum that returns the log message obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,17 +17,17 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
-    """                                                                                                                                               
-    Filters a log line by redacting specified fields.                                                                                                 
-                                                                                                                                                      
-    Args:                                                                                                                                             
-        fields (List[str]): The fields to redact.                                                                                                     
-        redaction (str): The string to replace the field values with.                                                                                 
-        message (str): The log message to filter.                                                                                                     
-        separator (str): The separator used in the log message.                                                                                       
-                                                                                                                                                      
-    Returns:                                                                                                                                          
-        str: The filtered log message.                                                                                                                
+    """
+    Filters a log line by redacting specified fields.
+
+    Args:
+        fields (List[str]): The fields to redact.
+        redaction (str): The string to replace the field values with.
+        message (str): The log message to filter.
+        separator (str): The separator used in the log message.
+
+    Returns:
+        str: The filtered log message.
     """
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
