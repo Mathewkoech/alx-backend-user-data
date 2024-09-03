b8e8a59334e84103cb99f5fdd13021f3f10b6e53
mathewkoech <mathewkoech>
2024-09-03 14:53:37 +0000

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 from flask import Flask, jsonify
-
+"""
+Route module for the API
+"""
 app = Flask(__name__)
 
 @app.errorhandler(401)
-def unauthorized_error(error):
-    response = jsonify({"error": "Unauthorized"})
-    response.status_code = 401
-    return response
+def unauthorized(error) -> str:
+     """ Unauthorized handler
+     """
+    return jsonify({"error": "Unauthorized"})401
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+     """ forbidden handler                                                                                      
+     """
+    return jsonify({"error": "Forbidden"})403
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+     """ forbidden handler                                                                                                   
+     """
+    return jsonify({"error": "Not found"})404
+
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,8 +1,20 @@
-#!/usr/bin/env python3
-from flask import Blueprint, abort
+"""Module of Index views.
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
 
-bp = Blueprint('index', __name__)
-
-@bp.route('/api/v1/unauthorized', methods=['GET'])
-def unauthorized():
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """GET /api/v1/unauthorized
+    Return:
+      - Unauthorized error.
+    """
     abort(401)
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return:
+      - Unauthorized error.
+    """
+    abort(403)
